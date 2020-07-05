63faeeb6e8446d7755088265c07c921f02ce29cb
Kayque Govetri <govetrikayque@gmail.com>
2020-07-05 14:12:47 -0300

    Novas models do sistema.

---
+++ src/models/Aluno.js
@@ -0,0 +1,22 @@
+import mongoose from '../database/index.js'
+
+const { Schema } = mongoose
+const ObjectId = Schema.Types.ObjectId
+const AlunoSchema = new Schema({
+
+  _turma: { type: ObjectId, ref: 'Turma', required: true },
+  nome: { type: String, required: true },
+  telefone: { type: String, default: '', required: true },
+  rm: { type: Number, default: 0, required: true},
+  dataNascimento: { type: Date, default: new Date(), required: true},
+  dataMatricula: {type: Date, default: new Date(), required: true},
+  cep: { type: String, default: '', required: true },
+  cpfResponsavel: { type: String, default: '', required: true },
+  email: { type: String, default: '', required: true },
+  senha: { type: String, default: '', required: true },
+
+}, { timestamps: true })
+
+const Aluno = mongoose.model('Aluno', AlunoSchema)
+
+export default Aluno 
