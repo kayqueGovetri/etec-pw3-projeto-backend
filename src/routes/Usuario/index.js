053e81fea7b7fcb0491b5192aa5aa6145d98bcb1
Marcus Primo <marcus.primov@gmail.com>
2020-07-09 22:48:33 -0300

    Rota /

--- src/routes/Usuario/index.js
+++ src/routes/Usuario/index.js
@@ -8,6 +8,6 @@ routes.get('/usuarios', auth, usuarioController.show)
 routes.post('/usuario', usuarioController.create)
 routes.delete('/usuario', auth, usuarioController.delete)
 routes.put('/usuario', auth, usuarioController.update)
-
+routes.get('/', (req,res) => {return res.status(200).json({teste : "teste"})})
 
 export default routes
